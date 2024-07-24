f59c0967ce1e9f3067795ba8235df24864faf6f8
Gina <gina.110716@gmail.com>
2024-07-24 10:03:43 +0800

    datatype_final

---
+++ L1_datatype.py
@@ -0,0 +1,41 @@
+#資料:程式碼基本單位
+#數字
+5
+12345
+5.13
+
+#字串
+"測試練習"
+"Hello typhoon"
+
+#布林值"
+True        #字首必大寫
+False
+
+#有順序、可變動列表 List
+[2, 6, 7]
+["Gina", "Alex", "Andy"]
+
+#有順序、不變動列表 Tuple
+(2, 5, 8)
+("Gina", "Alex")
+
+#集合(無順序) Set
+{4, 6, 9}
+{"Gina", "Joanna"}
+
+#字典 Dictionary
+{"apple":"red", "data":"資料"}
+
+#變數:用來儲存資料的自訂名稱
+#變數名稱 = 資料
+number = 5
+k = "Thursday~"
+p = {4, 6, 9}
+
+#print(data)
+print("Gina!")
+print([2, 6, 7])
+print(number)
+print(k)
+print(p)
