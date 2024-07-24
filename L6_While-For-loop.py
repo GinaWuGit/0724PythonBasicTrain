186425753875d83ee833c02b08b610a155c9d93c
Gina <gina.110716@gmail.com>
2024-07-24 14:18:15 +0800

    L6_loop_final

---
+++ L6_While-For-loop.py
@@ -0,0 +1,22 @@
+#1. while 迴圈
+# n = 1
+# sum = 0
+# while n < 11:
+#     print(n)
+#     sum += n
+#     n += 1
+# print("sum = ", sum)
+
+# 2. for 迴圈
+for x in range (9):     #不包含結尾
+    print(x)
+for y in "beef noodle":
+    print(y)
+for z in [12, 24, 36]:
+    print(z)
+
+# #ex: for
+# sum = 0
+# for x in range(1, 11):
+#     sum += x
+# print(sum)
